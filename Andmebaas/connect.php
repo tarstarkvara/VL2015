a2b2dd2f008421256148fd29c526dc057c821ab8
tarstarkvara <tarssander1@gmail.com>
2015-03-20 22:05:33 +0200

    Update connect.php

--- Andmebaas/connect.php
+++ Andmebaas/connect.php
@@ -14,4 +14,28 @@ function connect()
 	}
 	return $conn;
 }
+
+function addIsik($nimi, $perenimi, $isikukood, $kasutajanimi, $parool)
+{
+	$conn = connect();
+	$sql = "INSERT INTO isik (name, category, date, is_complete) VALUES (?, ?, ?, ?, ?)";
+	$stmt = $conn->prepare($sql);
+	$stmt->bindValue(1, $nimi);
+	$stmt->bindValue(2, $perenimi);
+	$stmt->bindValue(3, $isikukood);
+	$stmt->bindValue(4, $kasutajanimi);
+	$stmt->bindValue(5, $parool);
+	$stmt->execute();
+}
+
+function addHaaletus($algusaeg, $loppaeg, $nimi)
+{
+	$conn = connect();
+	$sql = "INSERT INTO isik (algusaeg, loppaeg, nimi) VALUES (?, ?, ?)";
+	$stmt = $conn->prepare($sql);
+	$stmt->bindValue(1, $algusaeg);
+	$stmt->bindValue(2, $loppaeg);
+	$stmt->bindValue(3, $nimi);
+	$stmt->execute();
+}
 ?>
