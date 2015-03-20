0bc1d67db98a4d7c14807ad284ff386c188a635d
tarstarkvara <tarssander1@gmail.com>
2015-03-20 22:24:48 +0200

    Update Loovaated.php

--- Andmebaas/Loovaated.php
+++ Andmebaas/Loovaated.php
@@ -2,18 +2,21 @@
 include_once 'connect.php';
 $conn = connect();
 
-$sql = "CREATE VIEW haaletuse_kandidaadid AS SELECT kandidaadid.kandidaadi_id, 
-        isik.nimi, isik.perenimi, kandidaadid.haaletuse_id from 
-        isik join kandidaadid on isik.id=kandidaadid.kandidaadi_id";
+$sql = "CREATE VIEW haaletuse_kandidaadid AS SELECT kandidaat.kandidaadi_id, 
+        isik.nimi, isik.perenimi, kandidaat.haaletuse_id from 
+        isik join kandidaat on isik.id=kandidaat.kandidaadi_id";
 
-$sql = "CREATE VIEW kaimas_haaletus AS SELECT id,nimi FROM haaletused group by loppaeg,algusaeg,id,nimi 
+$sql = "CREATE VIEW kaimas_haaletus AS SELECT id,nimi FROM haaletus group by loppaeg,algusaeg,id,nimi 
         having algusaeg<current_timestamp and loppaeg>current_timestamp";
 
-$sql = "CREATE VIEW loppenud_haaletus AS SELECT id,nimi FROM haaletused group by loppaeg,id,nimi 
+$sql = "CREATE VIEW loppenud_haaletus AS SELECT id,nimi FROM haaletus group by loppaeg,id,nimi 
         having loppaeg<current_timestamp";
         
-$sql = "CREATE VIEW tulemas_haaletus AS SELECT id,nimi FROM haaletused group by loppaeg,algusaeg,id,nimi 
+$sql = "CREATE VIEW tulemas_haaletus AS SELECT id,nimi FROM haaletus group by loppaeg,algusaeg,id,nimi 
         having algusaeg>current_timestamp";
+
+$sql = "CREATE VIEW haaletanuid AS SELECT haaletus.nimi, COUNT(*) FROM haal JOIN haaletus ON
+	haal.haaletuse_id=haaletus.haaletuse_id GROUP BY haaletus.nimi";
 
 try{
 	$conn->query($sql);
