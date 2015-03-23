60d8b970ab29e6e089540e9cc4bb13b004e34be9
tarstarkvara <tarssander1@gmail.com>
2015-03-23 22:19:25 +0200

    Create Lootabelid.php

---
+++ Andmebaas/Lootabelid.php
@@ -0,0 +1,37 @@
+<?php
+include_once 'connect.php';
+$conn = connect();
+$sql = "CREATE TABLE isik(
+			id INT NOT NULL AUTO_INCREMENT, 
+			PRIMARY KEY(id),
+			nimi VARCHAR(50),
+			perenimi VARCHAR(50),
+			isikukood INT,
+			kasutajanimi VARCHAR(50) UNIQUE NOT NULL,
+			parool VARCHAR(50) UNIQUE NOT NULL)";
+			
+$sql = "CREATE TABLE kandidaat(
+			kandidaadi_id INT NOT NULL, 
+			PRIMARY KEY(kandidaadi_id),
+			haaletuse_id INT)";
+			
+$sql = "CREATE TABLE haal(
+			id INT NOT NULL AUTO_INCREMENT, 
+			PRIMARY KEY(id),
+			haaletuse_id INT,
+			kandidaadi_id INT,
+			haaletaja_id INT)";
+$sql = "CREATE TABLE haaletus(
+			haaletuse_id INT NOT NULL AUTO_INCREMENT, 
+			PRIMARY KEY(haaletuse_id),
+			algusaeg DATE,
+			loppaeg DATE,
+			nimi VARCHAR(80))";
+try{
+	$conn->query($sql);
+}
+catch(Exception $e){
+	print_r($e);
+}
+echo "<h3>Tables created.</h3>";
+?>
